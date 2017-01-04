d7951b76461a80577fb23fec5b8202d73bfd4d3e
Alexandra <aolivares@icci.cl>
2017-01-04 12:06:01 -0300

    tipo mascota controller

--- app/controllers/tipo_mascota_controller.rb
+++ app/controllers/tipo_mascota_controller.rb
@@ -1,21 +1,42 @@
 class TipoMascotaController < ApplicationController
   before_action :set_tipo_mascotas, only: [:mostrar,:editar,:eliminar, :update]  
   def index
+    @tipomascota= Tipo_mascota.paginate(:page => params[:page], :per_page => 20).order('updated_at DESC')
   end
 
   def nuevo
+    @tipomascota= Tipo_mascota.new
   end
 
   def crear
+    @tipomascota = Tipo_mascota.new(tipo_mascota_params)
+    respond_to do |format|
+      if @tipomascota.save
+        format.html{redirect_to @tipomascota, notice:  'Tipo de mascota Guardado Con Exito'}
+      else
+        format.html{render :nuevo}
+      end
+    end
   end
 
   def update
+    respond_to do |format|
+      if @tipomascota.update(tipo_mascota_params)
+        format.html{redirect_to @tipomascota, notice: 'dueno Editado con Exito'}
+      else
+        format.html{render :edit}
+      end
+    end
   end
 
   def mostrar
   end
 
   def eliminar
+    @tipomascota.destroy 
+    respond_to do |format|
+      format.html { redirect_to duenos_url, notice: 'tipo de mascota eliminado con Exito.' }
+    end
   end
 
   def editar
